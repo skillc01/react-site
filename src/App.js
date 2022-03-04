cff68970721da0c0b391cbcd8c4fc3370534600f
Chloe <chloe.skillman@bbc.co.uk>
2022-03-04 14:13:35 +0000

    Create Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header(props) {
+  return(
+    <header>
+      <h1>{props.name} on the Web</h1>
+    </header>
+  )
+}
+
+function Main(props) {
+  return(
+    <section>
+      <p>{props.listen}</p>
+    </section>
+  )
+}
+
+function Footer(props) {
+  return(
+    <footer>
+      <p>Copyright {props.date}</p>
+    </footer>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Sounds"></Header>
+      <Main listen="Listen Live"/>
+      <Footer date={new Date().getFullYear()}/>
     </div>
   );
 }
